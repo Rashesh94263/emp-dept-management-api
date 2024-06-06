60b2f86cff60c7988cbbdbde53f02a6eec36386c
rashdobariya17 <96101461+rashdobariya17@users.noreply.github.com>
2024-06-06 14:21:13 -0700

    Implemented change in the name of the Service file

---
+++ src/main/java/com/example/employment_department_CURD_restful_services/EmploymentServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.employment_department_CURD_restful_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploymentDepartmentCurdRestfulServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploymentDepartmentCurdRestfulServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/employment_department_CURD_restful_services/EmploymentServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employment_department_CURD_restful_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploymentDepartmentCurdRestfulServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
